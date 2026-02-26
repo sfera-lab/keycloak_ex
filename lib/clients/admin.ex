7403b4da111c49c0ea2a78c3b5224d12ca4c6d04
Johann Mifsud <hello@johannmifsud.com>
2026-02-26 22:30:55 +0100

    feat: user

--- lib/clients/admin.ex
+++ lib/clients/admin.ex
@@ -54,6 +54,113 @@ defmodule KeycloakEx.Client.Admin do
         )
       end
 
+      defp post_request_realm(realm, url, body) do
+        conf = config()
+
+        OAuth2.Client.post(
+          new(),
+          "#{conf[:host_uri]}/admin/realms/#{realm}/#{url}",
+          body,
+          [
+            {"Authorization", "Bearer #{get_token().token.access_token}"},
+            {"Content-Type", "application/json"},
+            {"Accept", "application/json"}
+          ]
+        )
+      end
+
+      defp put_request_realm(realm, url, body) do
+        conf = config()
+
+        OAuth2.Client.put(
+          new(),
+          "#{conf[:host_uri]}/admin/realms/#{realm}/#{url}",
+          body,
+          [
+            {"Authorization", "Bearer #{get_token().token.access_token}"},
+            {"Content-Type", "application/json"},
+            {"Accept", "application/json"}
+          ]
+        )
+      end
+
+      defp delete_request_realm(realm, url) do
+        conf = config()
+
+        OAuth2.Client.delete(
+          new(),
+          "#{conf[:host_uri]}/admin/realms/#{realm}/#{url}",
+          [
+            {"Authorization", "Bearer #{get_token().token.access_token}"},
+            {"Accept", "application/json"}
+          ]
+        )
+      end
+
+      @doc "Creates a Keycloak user. Returns {:ok, user_id} on success, {:error, reason} on failure."
+      def create_user(realm, attrs) do
+        case post_request_realm(realm, "users", attrs) do
+          {:ok, %OAuth2.Response{status_code: 201, headers: headers}} ->
+            case List.keyfind(headers, "location", 0) do
+              {_, location} -> {:ok, location |> String.split("/") |> List.last()}
+              nil -> {:error, "No Location header in create_user response"}
+            end
+
+          {:ok, %OAuth2.Response{status_code: 409}} ->
+            {:error, "User already exists in Keycloak"}
+
+          {:ok, %OAuth2.Response{status_code: status}} ->
+            {:error, "create_user unexpected status: #{status}"}
+
+          {:error, reason} ->
+            {:error, reason}
+        end
+      end
+
+      @doc "Triggers Keycloak to send an execute-actions email (e.g. UPDATE_PASSWORD) to the given user."
+      def execute_actions_email(realm, user_id, actions, opts \\ []) do
+        redirect_uri = opts[:redirect_uri]
+        client_id = opts[:client_id]
+
+        qs =
+          URI.encode_query(
+            Enum.reject(
+              [redirect_uri: redirect_uri, client_id: client_id],
+              fn {_, v} -> is_nil(v) end
+            )
+          )
+
+        url =
+          if qs != "",
+            do: "users/#{user_id}/execute-actions-email?#{qs}",
+            else: "users/#{user_id}/execute-actions-email"
+
+        case put_request_realm(realm, url, actions) do
+          {:ok, %OAuth2.Response{status_code: 204}} ->
+            :ok
+
+          {:ok, %OAuth2.Response{status_code: status}} ->
+            {:error, "execute_actions_email unexpected status: #{status}"}
+
+          {:error, reason} ->
+            {:error, reason}
+        end
+      end
+
+      @doc "Deletes a Keycloak user. Returns :ok or {:error, reason}. Used for compensating rollback."
+      def delete_user(realm, user_id) do
+        case delete_request_realm(realm, "users/#{user_id}") do
+          {:ok, %OAuth2.Response{status_code: status}} when status in [204, 404] ->
+            :ok
+
+          {:ok, %OAuth2.Response{status_code: status}} ->
+            {:error, "delete_user unexpected status: #{status}"}
+
+          {:error, reason} ->
+            {:error, reason}
+        end
+      end
+
       def get_clients(realm) do
         get_request_realm(realm, "clients")
       end
